d363abe13f0c73db218d25a48425c31b53523200
Raf-Batista <RafaelB2424@gmail.com>
2020-07-01 23:25:46 -0400

    Create User and Boardgame base model along with some properties

---
+++ BGTracker.API/Models/BoardGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGTracker.API.Models
+{
+    public class BoardGame
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public bool WishList { get; set; } // If true this BoardGame is on the user's wishlist, otherwise the user owns it
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
